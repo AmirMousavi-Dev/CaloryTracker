d0bfec3b80bf55a6e76ac4ca8a26228860679919
Amirhossein <119420193+AmirMousavi-dev@users.noreply.github.com>
2024-02-28 15:53:13 +0330

    create dependencies objects

---
+++ buildSrc/src/main/java/AndroidX.kt
@@ -0,0 +1,8 @@
+object AndroidX {
+    private const val coreKtxVersion = "1.12.0"
+    const val coreKtx = "androidx.core:core-ktx:$coreKtxVersion"
+
+    private const val appCompatVersion = "1.6.1"
+    const val appCompat = "androidx.appcompat:appcompat:$appCompatVersion"
+
+}
---
+++ buildSrc/src/main/java/Google.kt
@@ -0,0 +1,4 @@
+object Google {
+    private const val materialVersion = "1.11.0"
+    const val material = "com.google.android.material:material:$materialVersion"
+}
